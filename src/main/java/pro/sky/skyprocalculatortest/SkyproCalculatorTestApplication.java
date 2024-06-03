618a68200215205648d87fad496336e270464309
original <git@github.com:denis-poroshin/variables.git>
2024-06-03 18:42:22 +0300

    Начало

---
+++ src/main/java/pro/sky/skyprocalculatortest/SkyproCalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocalculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCalculatorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprocalculatortest/SkyproCalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocalculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCalculatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
